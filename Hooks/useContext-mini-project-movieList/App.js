ee3335ef4ea8e73e85f63a41fb756ca9d7c62aa4
Ugurcan Turk <72524087+uct-trk@users.noreply.github.com>
2021-04-19 22:24:00 +0300

    useContext mini project movieList

---
+++ Hooks/useContext-mini-project-movieList/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import React from 'react'
+import MovieList from './MovieList'
+import Nav from './Nav'
+import {MovieProvider} from './MovieContext'
+import AddMovie from './AddMovie'
+
+function App(){
+
+  
+  return(
+    <MovieProvider>
+    <div className="App">
+      <Nav/>
+      <AddMovie/>
+      <MovieList/>
+    </div>
+    </MovieProvider>
+  )
+}
+
+export default App
---
+++ Hooks/useContext-mini-project-movieList/MovieList.js
@@ -0,0 +1,23 @@
+import React, { useContext, useState } from 'react'
+import Movie from './Movie'
+import {MovieContext} from './MovieContext'
+
+const MovieList = () => {
+
+  const [movies, setMovies] = useContext(MovieContext)
+
+  return(
+    <div>
+      
+    {
+      movies.map(movie => (
+        <Movie 
+          name={movie.name}
+          price={movie.price}
+          key={movie.id}/>
+      ))
+    } 
+    </div>
+  )
+}
+export default MovieList
